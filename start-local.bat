718964b00ef6210df4bd3bb0ff20e2dfb1603cba
MahadAshraf12 <mahad.ashraf@exitech.org>
2025-08-13 09:59:29 +0500

    final comit

---
+++ start-local.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Starting SaunaBot 5000 on localhost...
+echo.
+echo Make sure you have:
+echo 1. Created a .env file from env.example
+echo 2. Added your STORE_DOMAIN and OPENAI_API_KEY to .env
+echo.
+echo Starting server on http://localhost:3000
+echo.
+npm start
